4c3b6b548fe503ea7814865bd2781229aad34862
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-15 08:20:49 +0700

    monolithic programming

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// type basic
-string pesan = "Tahun Lahir";
-string finalPesan = pesan.ToUpper();
-int tahunLahir = 2001;
-string tahunLahirr = tahunLahir.ToString();
-Console.WriteLine(finalPesan + " " + tahunLahirr);
+// monolithic programming
 
-int satuKM = 900;
-bool isItAKM = satuKM >= 1000;
-if (isItAKM)
+int i, n;
+bool isPrime = true;
+Console.WriteLine("Enter a Positive Integer: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+// 0 and 1 are not prime numbers
+if (n == 0 || n == 1)
 {
-  Console.WriteLine("Ini 1KM atau bahkan lebih");
+  isPrime = false;
 }
+else
+{
+  for (i = 2; i <= n / 2; ++i)
+  {
+    if (n % i == 0)
+    {
+      isPrime = false;
+      break;
+    }
+  }
+}
+if (isPrime)
+{
+  Console.WriteLine($"{n} is a Prime Number");
+}
+else
+{
+  Console.WriteLine($"{n} is not a Prime Number");
+}
+
+// type basic
+// string pesan = "Tahun Lahir";
+// string finalPesan = pesan.ToUpper();
+// int tahunLahir = 2001;
+// string tahunLahirr = tahunLahir.ToString();
+// Console.WriteLine(finalPesan + " " + tahunLahirr);
+
+// int satuKM = 900;
+// bool isItAKM = satuKM >= 1000;
+// if (isItAKM)
+// {
+//   Console.WriteLine("Ini 1KM atau bahkan lebih");
+// }
 
 // Division
 // int num = 2 / 3;
@@ -21,13 +53,13 @@ if (isItAKM)
 // Console.WriteLine(c);
 
 // Overflow
-int num = int.MinValue;
-num--;
-Console.WriteLine(num == int.MaxValue);
-int num2 = int.MaxValue;
-num2++;
-Console.WriteLine(num2 == int.MinValue);
+// int num = int.MinValue;
+// num--;
+// Console.WriteLine(num == int.MaxValue);
+// int num2 = int.MaxValue;
+// num2++;
+// Console.WriteLine(num2 == int.MinValue);
 
-int num3 = 1000000;
-int num4 = 1000000;
-Console.WriteLine(checked(num3 * num4));
+// int num3 = 1000000;
+// int num4 = 1000000;
+// Console.WriteLine(checked(num3 * num4));
