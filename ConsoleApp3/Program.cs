e3e2eb88a9a44e2fe83ffe8aaca7e7d0e3a484d0
Rifai <rifaiabdurrohman8@gmail.com>
2025-07-15 12:02:21 +0700

    initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// bitwise operators
+int a = 13, b = 25, result;
+// bitwise AND operator
+result = a & b;
+Console.WriteLine($"Bitwise AND: {result}");
+// bitwise OR operator
+result = a | b;
+Console.WriteLine($"Bitwise OR: {result}");
+// bitwise XOR operator
+result = a ^ b;
+Console.WriteLine($"Bitwise XOR: {result}");
+
+
+// relational operators
+bool isResult;
+int num1 = 10, num2 = 30;
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+// equal to operator
+isResult = (num1 == num2);
+Console.WriteLine($"equal (=) to operator {isResult}");
+// greater than operator
+isResult = (num1 > num2);
+Console.WriteLine($"greater than (>) operator: {isResult}");
+// less than operator
+isResult = (num1 < num2);
+Console.WriteLine($"less than (<) operator: {isResult}");
+// greater than equal to operator
+isResult = (num1 >= num2);
+Console.WriteLine($"greater than or equal to (>=) operator: {isResult}");
+// less than equal to operator
+isResult = (num1 <= num2);
+Console.WriteLine($"less than or equal to (<=) operator: {isResult}");
+// not equal to operator
+isResult = (num1 != num2);
+Console.WriteLine($"not equal to (!=) operator: {isResult}");
